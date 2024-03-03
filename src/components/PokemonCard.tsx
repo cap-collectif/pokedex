04621b11f561e64e2468a158061390a089a69367
Matthieu Franck <matthieufranck@Matthieu-Franck.lan>
2024-03-03 23:05:38 +0100

    added more styling

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -13,14 +13,19 @@ interface PokemonCardProps {
 }
 
 const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
-  const { name, weight, height, color } = pokemon
+  const { name, weight, height, color, pokemonId } = pokemon
 
   return (
-    <div>
+    <div
+      className="border-8 w-64 h-96 rounded-2xl border-yellow-200 p-2"
+      style={{ background: 'rgba(255, 0, 0, 0.3)' }}
+    >
       <h2>{name}</h2>
+      <div className=" w-full  bg-white h-36 overflow-hidden flex justify-center items-center">
+        <img src={`https://img.pokemondb.net/artwork/${name}.jpg`} alt="" className=" w-full h-full object-contain" />
+      </div>
       <p>Weight: {weight ?? 'N/A'}</p>
       <p>Height: {height ?? 'N/A'}</p>
-      <p>Color: {color ?? 'white'}</p>
     </div>
   )
 }
