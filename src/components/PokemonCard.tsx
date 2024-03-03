728211afde230fdbdea31071ac8ef1c0f4491e74
Matthieu Franck <matthieufranck@Matthieu-Franck.lan>
2024-03-03 23:37:15 +0100

    change styling card

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 interface Pokemon {
@@ -5,7 +6,7 @@ interface Pokemon {
   name: string | null | undefined
   weight: number | null | undefined
   height: number | null | undefined
-  color: string | null | undefined
+  color: string
 }
 
 interface PokemonCardProps {
@@ -16,17 +17,22 @@ const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
   const { name, weight, height, color, pokemonId } = pokemon
 
   return (
-    <div
+    <Link
+      href={`/pokemon?id=${pokemonId}`}
       className="border-8 w-64 h-96 rounded-2xl border-yellow-200 p-2"
-      style={{ background: 'rgba(255, 0, 0, 0.3)' }}
+      style={{ backgroundColor: color }}
     >
       <h2>{name}</h2>
-      <div className=" w-full  bg-white h-36 overflow-hidden flex justify-center items-center">
-        <img src={`https://img.pokemondb.net/artwork/${name}.jpg`} alt="" className=" w-full h-full object-contain" />
+      <div className=" shadow-md w-full  bg-white h-36 overflow-hidden flex justify-center items-center">
+        <img
+          src={`https://img.pokemondb.net/artwork/${name}.jpg`}
+          alt={`image of ${name}`}
+          className=" w-full h-full object-contain"
+        />
       </div>
       <p>Weight: {weight ?? 'N/A'}</p>
       <p>Height: {height ?? 'N/A'}</p>
-    </div>
+    </Link>
   )
 }
 
