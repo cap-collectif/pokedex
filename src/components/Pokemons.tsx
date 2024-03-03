df3037413d00c392151113cf900d99f46d1d38a0
Matthieu Franck <matthieufranck@Matthieu-Franck.lan>
2024-03-03 14:37:15 +0100

    added a rudimentary searchbar

--- src/components/Pokemons.tsx
+++ src/components/Pokemons.tsx
@@ -2,27 +2,31 @@
 import { graphql, useLazyLoadQuery } from 'react-relay'
 import { PokemonsQuery } from '../../__generated__/PokemonsQuery.graphql'
 import Link from 'next/link'
-
-const GRAPHQL = graphql`
-  query PokemonsQuery {
-    pokemons: pokemon_v2_pokemonspecies(limit: 11, where: { name: { _ilike: "%" } }) {
-      name
-      pokemonId: id
-      pokemon_v2_pokemons {
-        weight
-        height
-      }
-      pokemonColor: pokemon_v2_pokemoncolor {
-        pokemonId: id
-        name
-      }
-    }
-  }
-`
+import { useState } from 'react'
 
 // TODO : Add a bit of styling
 export const Pokemons = () => {
-  const data = useLazyLoadQuery<PokemonsQuery>(GRAPHQL, {})
+  const [search, setSearch] = useState('')
+  const searchapi = `${search}%`
+
+  const GRAPHQL = graphql`
+    query PokemonsQuery($searchapi: String!) {
+      pokemons: pokemon_v2_pokemonspecies(limit: 11, where: { name: { _ilike: $searchapi } }) {
+        name
+        pokemonId: id
+        pokemon_v2_pokemons {
+          weight
+          height
+        }
+        pokemonColor: pokemon_v2_pokemoncolor {
+          pokemonId: id
+          name
+        }
+      }
+    }
+  `
+
+  const data = useLazyLoadQuery<PokemonsQuery>(GRAPHQL, { searchapi })
 
   // To help
   console.log(data)
@@ -30,17 +34,16 @@ export const Pokemons = () => {
   return (
     <div className="p-4">
       <h1 className="mb-5">Pokemons :</h1>
+      <input type="text" value={search} onChange={e => setSearch(e.target.value)} placeholder="Search for a pokemon" />
       <div className="grid grid-cols-4 gap-4">
-        {/* {data.pokemons.map(pokemon => {
-          const sprite = pokemon?.sprites[0]?.sprites?.front_default
-          return (
-            <div key={pokemon.pokemonId}>
-              <div>Name: {pokemon.name}</div>
-              <img src={sprite} alt={pokemon.name} />
-              <Link href={`/pokemon?id=${pokemon.pokemonId}`}>Infos</Link>
-            </div>
-          )
-        })} */}
+        {data.pokemons.map(pokemon => (
+          <div key={pokemon.pokemonId}>
+            <h2>{pokemon.name}</h2>
+            <p>Weight: {pokemon.pokemon_v2_pokemons[0].weight}</p>
+            <p>Height: {pokemon.pokemon_v2_pokemons[0].height}</p>
+            <p>Color: {pokemon.pokemonColor.name}</p>
+          </div>
+        ))}
       </div>
     </div>
   )
